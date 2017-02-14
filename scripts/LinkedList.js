28bf3d99e3a1f041797fdccfc5b2b38ee4a735ee
Hugh Morrison <hmorri32@gmail.com>
2017-02-14 12:30:08 -0700

    first test passing linked list

---
+++ scripts/LinkedList.js
@@ -0,0 +1,15 @@
+import ListNode from '../scripts/ListNode.js'
+
+
+class List {
+  constructor(){
+    this._length = 0;
+    this.head = null;
+  }
+  push(data){
+    
+  }
+
+}
+
+export default List
